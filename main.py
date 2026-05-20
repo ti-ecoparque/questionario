893c9f16ef76e88b0a31c78cf7243deac06e481d
ti-ecoparque <ti.cascavel@ecoparque.com.br>
2026-05-20 09:53:57 -0300

    ajustando grupo um de perguntas

--- main.py
+++ main.py
@@ -85,29 +85,29 @@ else:
 
     # --- PERGUNTA 1 ---
     # Texto em caixa alta
-    txt_p01 = "Pergunta 1: Eu compreendo claramente quais são as minhas responsabilidades diárias."
+    txt_p01 = "Pergunta 1: Sei exatamente quais são minhas responsabilidades no trabalho."
     # Markdown com HTML (Aumenta o tamanho usando font-size)
     st.markdown(f"<p style='font-size:18px; font-weight:bold;'>{txt_p01.upper()}</p>", unsafe_allow_html=True)
     # st.radio agora fica sem texto interno, servindo apenas para exibir as opções
     p01 = st.radio("", lista_opcoes, index=2, key="rad_p01", label_visibility="collapsed")
 
     # --- PERGUNTA 2 ---
-    txt_p02 = "Pergunta 2: Sei exatamente o que a liderança espera do meu desempenho profissional."
+    txt_p02 = "As expectativas sobre meu desempenho são claras."
     st.markdown(f"<p style='font-size:18px; font-weight:bold;'>{txt_p02.upper()}</p>", unsafe_allow_html=True)
     p02 = st.radio("", lista_opcoes, index=2, key="rad_p02", label_visibility="collapsed")
 
     # --- PERGUNTA 3 ---
-    txt_p03 = "Pergunta 3: Os objetivos e metas do meu cargo são definidos de forma clara."
+    txt_p03 = "Recebo orientações claras sobre como executar minhas atividades."
     st.markdown(f"<p style='font-size:18px; font-weight:bold;'>{txt_p03.upper()}</p>", unsafe_allow_html=True)
     p03 = st.radio("", lista_opcoes, index=2, key="rad_p03", label_visibility="collapsed")
 
     # --- PERGUNTA 4 ---
-    txt_p04 = "Pergunta 4: Entendo como o meu trabalho diário contribui para o sucesso da empresa."
+    txt_p04 = "Sei a quem recorrer quando tenho dúvidas sobre minhas tarefas."
     st.markdown(f"<p style='font-size:18px; font-weight:bold;'>{txt_p04.upper()}</p>", unsafe_allow_html=True)
     p04 = st.radio("", lista_opcoes, index=2, key="rad_p04", label_visibility="collapsed")
 
     # --- PERGUNTA 5 ---
-    txt_p05 = "Pergunta 5: Existe uma divisão justa de tarefas dentro da minha equipe de trabalho."
+    txt_p05 = "Pergunta 5: Mudanças nas minhas funções são comunicadas de forma clara."
     st.markdown(f"<p style='font-size:18px; font-weight:bold;'>{txt_p05.upper()}</p>", unsafe_allow_html=True)
     p05 = st.radio("", lista_opcoes, index=2, key="rad_p05", label_visibility="collapsed")
 
